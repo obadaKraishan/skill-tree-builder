001a0f99bf9f70aa566587bd03126446263c2b60
obada kraishan <obada.kraishan@gmail.com>
2024-08-22 21:26:46 -0500

    Enhance the Skill Trees Nodes

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const apiRoutes = require('./api');
+const skillTreeRoutes = require('./routes/skillTreeRoutes');
 const cors = require('cors');
 require('dotenv').config();
 
@@ -9,7 +9,7 @@ const app = express();
 app.use(cors());
 
 app.use(express.json());
-app.use('/api', apiRoutes);
+app.use('/api', skillTreeRoutes);
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
